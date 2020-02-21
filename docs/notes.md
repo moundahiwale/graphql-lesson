8c0a5e571a1ab93f650f1a65acca907d311a4add
Moun Dahiwale <moun.dahiwale@hmhco.com>
2020-02-21 14:17:26 +0000

    Introduction to Apollo

---
+++ docs/notes.md
@@ -0,0 +1,3 @@
+Install required packages:  
+yarn add apollo-boost graphql react-apollo  
+
--- src/index.js
+++ src/index.js
@@ -1,21 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+// Allows rest of the app to access the state that is stored on Apollo
+import { ApolloProvider } from "react-apollo";
+// Connects client to the /graphql server
+import { createHttpLink } from "apollo-link-http";
+// Apollo uses this to cache the data it fetches
+import { InMemoryCache } from "apollo-cache-inmemory";
+// Is a bundle of smaller libraries including apollo-link-http & apollo-cache-inmemory
+import { ApolloClient, gql } from "apollo-boost";
 
-import { store, persistor } from './redux/store';
+import { store, persistor } from "./redux/store";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
+
+const httpLink = createHttpLink({
+  uri: "https://crwn-clothing.com/"
+});
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({ link: httpLink, cache });
+
+// Using client to fetch data
+client
+  .query({
+    query: gql`
+      {
+        collections {
+          id
+          title
+          items {
+            price
+            name
+          }
+        }
+      }
+    `
+  })
+  .then(res => console.log(res));
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+      </BrowserRouter>
+    </Provider>
+  </ApolloProvider>,
+  document.getElementById("root")
 );
