d3c368910ee6673ad07b3988c0decb4cd542386a
Moun Dahiwale <moun.dahiwale@hmhco.com>
2020-03-23 13:17:26 +0000

    Update collections overview to use Apollo GraphQL instead of redux

---
+++ src/components/collections-overview/collections-overview.container.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { gql } from "apollo-boost";
+import { Query } from "react-apollo";
+import CollectionsOverview from "./collections-overview.component";
+import Spinner from "../spinner/spinner.component";
+
+const GET_COLLECTIONS = gql`
+  {
+    collections {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`;
+
+const CollectionsOverviewContainer = () => {
+  return (
+    <Query query={GET_COLLECTIONS}>
+      {({ loading, data }) => {
+        if (loading) return <Spinner />;
+        return <CollectionsOverview collections={data.collections} />;
+      }}
+    </Query>
+  );
+};
+
+export default CollectionsOverviewContainer;
